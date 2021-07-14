405caaee1dd12b7c0654e6932987c0d421c22352
Anthony-ndegwa <anthony.n.ndegwa@gmail.com>
2021-07-14 13:46:04 +0300

    Add files via upload

---
+++ js/animation.js
@@ -0,0 +1,36 @@
+// Slide In
+const content = document.querySelectorAll(".about-container, .experience-container");
+
+window.addEventListener("scroll", easeContent);
+easeContent();
+
+function easeContent() {
+	const bottomOfPage = window.innerHeight * 0.7;
+
+	content.forEach((contents) => {
+		const topOfContent = contents.getBoundingClientRect().top;
+		if (topOfContent < bottomOfPage) {
+			contents.classList.add("appear")
+		} else {
+			contents.classList.remove("appear")
+		}
+	})
+}
+
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+	var i;
+	var x = document.getElementsByClassName("mySlides");
+	for (i = 0; i < x.length; i++) {
+		x[i].style.display = "none";
+	}
+	slideIndex++;
+	if (slideIndex > x.length) {
+		slideIndex = 1
+	}
+	x[slideIndex-1].style.display = "block";
+	setTimeout(carousel, 2000);
+}
+// End
